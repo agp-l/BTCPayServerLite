66913ee0891a684cc925b6db6b533da23fbf4dcf
AGP[L] <62847503+agp-l@users.noreply.github.com>
2026-08-31 10:47:50 +0200

    Use generic authentication attempt schema

--- sql.sql
+++ sql.sql
@@ -16,13 +16,13 @@ CREATE TABLE `users` (
   DEFAULT CHARSET=utf8mb4
   COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `auth_login_attempts` (
+CREATE TABLE `auth_attempts` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `identity_hash` BINARY(32) NOT NULL,
     `attempted_at` BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `idx_auth_login_attempt_identity` (`identity_hash`, `attempted_at`),
-    KEY `idx_auth_login_attempt_cleanup` (`attempted_at`)
+    KEY `idx_auth_attempt_identity` (`identity_hash`, `attempted_at`),
+    KEY `idx_auth_attempt_cleanup` (`attempted_at`)
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4
   COLLATE=utf8mb4_unicode_ci;
