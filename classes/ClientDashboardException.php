05a39ec38d00152ee2ed7697385626aa0aa4dcd7
AGP[L] <62847503+agp-l@users.noreply.github.com>
2026-08-31 15:34:00 +0200

    Add client dashboard application exception

---
+++ classes/ClientDashboardException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BtcPayLite;
+
+use RuntimeException;
+use Throwable;
+
+final class ClientDashboardException extends RuntimeException
+{
+    private int $httpStatus;
+
+    public function __construct(string $message, int $httpStatus = 400, ?Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+        $this->httpStatus = $httpStatus;
+    }
+
+    public function getHttpStatus(): int
+    {
+        return $this->httpStatus;
+    }
+}
