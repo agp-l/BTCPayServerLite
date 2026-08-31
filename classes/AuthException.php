a61f2c24fc13ed89fb3439e87e4364733c959418
AGP[L] <62847503+agp-l@users.noreply.github.com>
2026-08-31 10:29:13 +0200

    Add safe authentication exception

---
+++ classes/AuthException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BtcPayLite;
+
+use RuntimeException;
+
+/**
+ * A user-safe authentication or session error.
+ */
+class AuthException extends RuntimeException
+{
+}
